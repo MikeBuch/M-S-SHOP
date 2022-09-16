122ab63ccfe7e8cf738537844ce46b2705828447
MikeBuch <buch98199@gmail.com>
2022-09-16 11:24:27 +0300

    added home page

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js',
     
   ])
